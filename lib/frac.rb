3e176faee8a50173c861fac733f2b210b3df1b3c
alejando David Carrillo Padrón <alu0100845808@ull.edu.es>
2015-10-29 11:22:32 +0000

    inicializacion

--- lib/frac.rb
+++ lib/frac.rb
@@ -1,14 +1,9 @@
-#byebyeWorld.rb
-require 'pry'
+Class fracciones
 
- #define a method
- def byebye() puts "bye bye world!!!" end
-
- #set x to 10
- x = 10
-
- #start a REPL session
- binding.pry
-
- #program resumes here (after pry session)
-  puts "program resumes here. Value of x is: #{x}."
+ 
+ 
+ 
+ 
+ 
+ 
+end
---
+++ test/unitari.rb
@@ -0,0 +1,12 @@
+# File:  tc_simple_number.rb
+
+require_relative "frac"
+require "test/unit"
+ 
+class TestSimpleNumber < Test::Unit::TestCase
+ 
+  def test_simple
+    
+  end
+ 
+end
