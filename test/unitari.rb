df790cb2423a2a23a85d3f1b327d30a666383989
alejando David Carrillo Padrón <alu0100845808@ull.edu.es>
2015-10-29 12:35:15 +0000

    primera error

--- test/unitari.rb
+++ test/unitari.rb
@@ -1,12 +1,12 @@
-# File:  tc_simple_number.rb
-
-require_relative "frac"
-require "test/unit"
+require 'test/unit'
  
 class TestSimpleNumber < Test::Unit::TestCase
- 
+  def setup
+  @dostercios=Fracciones.new(2,3)      
+  end
   def test_simple
-    
+    @dostercios.denom
+    @dostercios.num
   end
  
 end
