11720b247ad05fdddf86ca7b0d67e9edf3747f6f
alejando David Carrillo Padrón <alu0100845808@ull.edu.es>
2015-10-30 10:43:21 +0000

    Finalizado

--- test/unitari.rb
+++ test/unitari.rb
@@ -7,26 +7,37 @@ class TestFracciones < Test::Unit::TestCase
   @cincotercios=Fraccion.new(5,3)
   end
   
-  def test_suma
+  def create
     assert_equal("2/3", @dostercios.to_s)
+  end
+  
+  def test_suma
     assert_equal("7/3", (@dostercios+@cincotercios).to_s)
   end
   
   
   def test_resta
-    #assert_equal("7/3", (@cincotercios-@dostercios).to_s)
+    assert_equal("1/1", (@cincotercios-@dostercios).to_s)
     
   end
   
   
   def test_mult
-  
+  assert_equal("10/9", (@cincotercios*@dostercios).to_s)
   
   end
   
   def test_div
-    
+    assert_equal("5/2", (@cincotercios/@dostercios).to_s)
   
+  end
+  
+  def test_fails
+    #assert_equal("6/3", (@dostercios+@cincotercios).to_s)
+    #assert_equal("7/3", (@dostercios-@cincotercios).to_s)
+    #assert_equal("7/3", (@dostercios*@cincotercios).to_s)
+    #assert_equal("7/3", (@dostercios/@cincotercios).to_s)
+    @dostercios=Fraccion.new('a',3)
   end
   
   
